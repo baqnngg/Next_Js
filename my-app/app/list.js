c1470198dd94d28a76bcb76be0ba9539291f0e99
baqnngg <il8696546@gmail.com>
2024-07-23 14:52:24 +0900

    next_js

---
+++ my-app/app/list.js
@@ -0,0 +1,8 @@
+export default function List() {
+    return (
+      <div>
+        <h4 className="title">상품</h4>
+      </div>
+    );
+  }
+  
---
+++ my-app/app/list/1/page.js
@@ -0,0 +1,7 @@
+export default function Product(){
+    return (
+        <div>
+            <div className="food">상품 1</div>
+        </div>
+    )
+}
