f2259541a145beb234acd0ec331ff6f265150024
baqnngg <il8696546@gmail.com>
2024-07-24 09:27:53 +0900

    buttonC

--- my-app/app/list/page.js
+++ my-app/app/list/page.js
@@ -1,23 +1,33 @@
 'use client'
 
-export default function List(){
-    let 상품 = ['Tomatoes', 'Pizza', 'Pasta']
+import { useState } from 'react';
+
+export default function List() {
+    let 상품 = ['Tomatoes', 'Pizza', 'Pasta'];
+
+    const [quantities, setQuantities] = useState(Array(상품.length).fill(1));
+
+    const increaseQuantity = (index) => {
+        const newQuantities = [...quantities];
+        newQuantities[index] += 1;
+        setQuantities(newQuantities);
+    };
+
     return (
         <div>
             <h4 className="title">상품 목록</h4>
             {
-                상품.map((a,i) => {
-                    return(
+                상품.map((a, i) => {
+                    return (
                         <div className="food" key={i}>
-                            <img src={`/food${i}.png`} className="food-img"></img>
+                            <img src={`/food${i}.png`} className="food-img" alt={`${a}`} />
                             <h4>{a} 14000원</h4>
-                            <span>1</span>
-                            <button onClick={() =>(console.log(1))}>+</button>
+                            <span>{quantities[i]}</span>
+                            <button onClick={() => increaseQuantity(i)}>+</button>
                         </div>
-                    )
+                    );
                 })
             }
-
         </div>
-    )
+    );
 }
