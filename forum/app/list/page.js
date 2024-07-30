70aa71c3573477f0f02a17dff922e71e0b5b5d6d
unknown <il8696546@gmail.com>
2024-07-30 13:37:55 +0900

    DB

--- forum/app/list/page.js
+++ forum/app/list/page.js
@@ -4,13 +4,16 @@ import Link from 'next/link';
 export default async function List() {
     let db = (await connectDB).db('forum');
     let result = await db.collection('post').find().toArray();
+    let time = Date()
+
     return(
         <div className="list-bg">
             {
                 result.map((a,i) => 
                 <div className="list-item" key={i}>
                     <Link href={'/detail/'+result[i]._id}><h4>{result[i].title}</h4></Link>
-                    <p>1월 1일</p>
+                    <Link href={'/edit/'+result[i]._id} className="list-btn">🐔</Link>
+                    <p>{time}</p>
                 </div>
                 )
             }
--- forum/app/test.js
+++ forum/app/test.js
@@ -1,5 +1,5 @@
 import { MongoClient } from "mongodb";
 
-const client = await MongoClient.connect('mongodb+srv://admin123:<wkdqudwns>@jangbyenogjoun.rokxrph.mongodb.net/?retryWrites=true&w=majority&appName=JangByenogJoun  ',{userNewUrlParser:true});
+const client = await MongoClient.connect('mongodb+srv://admin123:1234@jangbyenogjoun.rokxrph.mongodb.net/?retryWrites=true&w=majority&appName=JangByenogJoun  ',{userNewUrlParser:true});
 const db = client.db("forum");
 db.collection('post').find();
